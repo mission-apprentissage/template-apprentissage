8ec69668b371993722bcca18b9aaae6af2c33301
Kevin Barnoin <k.barnoin@gmail.com>
2022-04-06 10:52:22 +0200

    fix: missing semicolon

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,21 +1,22 @@
-
-function inline(value){
-  return value.replace(/\s{2,}/g, ' ').trim()
+function inline(value) {
+  return value.replace(/\s{2,}/g, " ").trim();
 }
 
 const contentSecurityPolicy = `
       default-src 'self';
-      base-uri 'self'
+      base-uri 'self';
       block-all-mixed-content;
       font-src 'self' https: data:;
       frame-ancestors 'self';
       img-src 'self' data:;
       object-src 'none';
-      script-src 'self' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'": ""};
+      script-src 'self' ${
+        process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""
+      };
       script-src-attr 'none';
       style-src 'self' https: 'unsafe-inline';
       upgrade-insecure-requests;
-`
+`;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -26,13 +27,15 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/:path*',
-        headers: [{
-          key: 'Content-Security-Policy',
-          value: inline(contentSecurityPolicy)
-        }],
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: inline(contentSecurityPolicy),
+          },
+        ],
       },
-    ]
+    ];
   },
 };
 
