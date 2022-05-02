f04e4aabf686c9b8c028d873760d756723e452b7
Benoît Guérout <bguerout@gmail.com>
2022-05-02 14:54:00 +0200

    chore: exporterUtils and fileUtils

--- server/src/common/utils/fileUtils.js
+++
@@ -1,6 +0,0 @@
-const csvToJson = require("convert-csv-to-json");
-
-const readJsonFromCsvFile = (localPath) => {
-  return csvToJson.getJsonFromCsv(localPath);
-};
-module.exports.readJsonFromCsvFile = readJsonFromCsvFile;
